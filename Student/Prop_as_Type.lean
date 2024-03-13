6c5924b4bffa74ec90bf44323c0e8b826ed6ca65
ash2taf <ash2taf@virginia.edu>
2024-03-13 16:14:39 +0000

    Class Wed 3.13

--- Student/Prop_as_Type.lean
+++ Student/Prop_as_Type.lean
@@ -51,9 +51,49 @@ example : BobStudiesatUVA × MaryStudiesatUVA → MaryStudiesatUVA := λ p => p.
 
 --Lean uses Sum for or, uses ⊕  '\o+'
 example : Sum BobStudiesatUVA MaryStudiesatUVA := Sum.inl b
-example : BobStudiesatUVA  ⊕ JimStudiesatUVA := Sum.inr _ --no proof of jim, would work with inl and b
+example : BobStudiesatUVA  ⊕ JimStudiesatUVA := Sum.inr sorry --no proof of jim, would work with inl and b
 
 --Proof of disjunction
 example : BobStudiesatUVA ⊕ JimStudiesatUVA → BobStudiesatUVA
 | Sum.inl l => l
 | Sum.inr r => nomatch r
+
+--proof of negation
+example : neg JimStudiesatUVA := λ p : JimStudiesatUVA => nomatch p
+
+example : neg (MaryStudiesatUVA × JimStudiesatUVA) := λ p : (MaryStudiesatUVA × JimStudiesatUVA) => nomatch p.2
+
+/-!
+Proof Irrelevance
+new versions of prod and sum for props, PProd and presumably PSum
+also MProd where α and β must come from the same universe
+Lean And takes 2 props, no more of this computational type nonsense
+
+need new props for new toys
+-/
+
+inductive B : Prop
+| paid
+| attend
+
+inductive M : Prop
+| paid
+| attend
+
+inductive K : Prop
+
+example : B ∧ M := And.intro B.paid M.attend
+def b_m_true : B ∧ M := And.intro B.paid M.attend
+theorem b_m_true' : B ∧ M := And.intro B.paid M.attend
+
+example : B ∧ M → M := λ p => p.right
+example : B ∧ M → B := λ p => p.1
+
+example : B ∧ ¬K := And.intro B.paid (λ k => nomatch k)
+example : B ∧ Not K := ⟨B.attend, λ k => nomatch k⟩
+
+example : B ∨ K := Or.inl B.attend
+example : B ∨ K → B :=
+λ bk => match bk with
+| Or.inl b => b
+| Or.inr k => nomatch k
