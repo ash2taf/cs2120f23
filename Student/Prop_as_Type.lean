b3d0fdd617fb6e6a8221ef172c607665492e5ca7
ash2taf <ash2taf@virginia.edu>
2024-03-18 16:15:21 +0000

    class Mon 3/18

--- Student/Prop_as_Type.lean
+++ Student/Prop_as_Type.lean
@@ -83,6 +83,7 @@ inductive M : Prop
 inductive K : Prop
 
 example : B ∧ M := And.intro B.paid M.attend
+example :  And B M := ⟨ B.paid, M.attend ⟩
 def b_m_true : B ∧ M := And.intro B.paid M.attend
 theorem b_m_true' : B ∧ M := And.intro B.paid M.attend
 
@@ -97,3 +98,45 @@ example : B ∨ K → B :=
 λ bk => match bk with
 | Or.inl b => b
 | Or.inr k => nomatch k
+
+example : ∀ (P Q R : Prop),
+(P ∨ Q) →
+(P → R) →
+(Q → R) → R :=
+λ _ _ _ porq pr qr => match porq with
+| Or.inl p => pr p
+| Or.inr q => qr q
+
+--Not
+--into: prove ¬P by showing P → False
+-- elim: as with any function, elim by apply
+def notK : ¬K := λ k => nomatch k
+
+--elim example (theorem/law of no contradiction - can't have P and ¬P)
+example (P : Prop): ¬P → P → False :=
+λ np p => np p
+
+/-!
+example (P : Prop) : ¬¬P → P
+|nnp => _ --no place to get a proof of p
+-/
+
+--but you can do it if all props are either true or false
+--this is the law of the excluded middle, that always P or ¬P for every proposition
+example (P : Prop) : (P ∨ ¬P) → (¬¬P → P) :=
+λ pornp => match pornp with
+  | Or.inl p => λ _ => p --technically can skip the lambda here?
+  | Or.inr np => λ nnp => nomatch (nnp np)
+
+--EM axiom:
+-- ∀(P : Prop), P ∨ ¬P
+--can't do proof by contradiction in lean because you don't have this
+
+-- Implication (P → Q)
+-- intro: show that you can derive a proof of Q from a proof of P
+-- elim: apply function to proof of P to get a proof of Q
+
+--intro example
+def notK' : K → False := λ k => nomatch k
+
+--Quantifiers
