beb5f23a6ee2eb0f9f61ca48c8ac32b10f97a06a
ash2taf <ash2taf@virginia.edu>
2024-03-25 16:23:43 +0000

    Class Mon 3/25

--- Student/Prop_as_Type.lean
+++ Student/Prop_as_Type.lean
@@ -190,3 +190,119 @@ example : 1=1 := Eq.refl 1
 --intro to exists: show an example/witness (exists a number even and prime: 2)
 -- or, pick a specific object and show that it meets the required properties (2 is even, 2 is prime)
 -- CANT get back the example that you used however
+
+--Predicates: parameterized propositions, applying to the correct arg(s) creates a prop
+-- You can think of predicates as describing a property, which elements of the taken type may or may not have
+
+def isEven (n:Nat) : Prop := n%2 = 0
+
+#check isEven 1
+#reduce isEven 3
+#reduce isEven 24
+
+
+/-!
+### For All (∀)
+in classical logic, universal quantified propsitions say ∀ p:P, Q, says P → Q
+
+more commonly, [∀ p:P, Q p], where Q is a predicate with an argument p.
+The overall prop is true if Q p is true for all values p of Type P.
+
+example using isEven with Nats: obviously false, says every natural number is even
+ie, [∀ n : Nat, isEven n], which is false
+-/
+
+--example: ∀ (n : Nat), isEven n := _ --not true, can't prove
+--this is often called a universal generalization
+
+/-!
+How to set about proving universal generalizations?
+pick arbitrary member of type under forall, show that it can get proof of predicate
+-/
+
+example : ∀ (n : Nat), n = 0 ∨ n ≠ 0 :=
+λ n => match n with
+| Nat.zero => Or.inl (Eq.refl 0)
+| Nat.succ _ => Or.inr (λ oeqz => nomatch oeqz)
+
+def zornz: ∀ (n : Nat), n = 0 ∨ n ≠ 0 :=
+λ n => match n with
+| Nat.zero => Or.inl (Eq.refl 0)
+| Nat.succ _ => Or.inr (λ oeqz => nomatch oeqz)
+
+#reduce zornz 3
+
+/-!
+#### Elimination
+if you have a proof of forall, you can apply to any instance to get a proof thereof
+this is called universal specialization
+ie, if you have a proof of [∀ p:P, Q p], then you have a proof of [Q p] for any p
+-/
+
+#reduce isEven 0
+#reduce isEven 1
+
+--this is dependent type theory, hot area/skill (don't frontline lean when bragging)
+
+--in Lean "varaible" introduces assumed objectsion into the environment
+
+variable
+  (P: Type)
+  (Q: P → Prop)
+  (R : Prop)
+  (t : P)
+
+#check P
+#check t
+#check Q t
+#check ∀ (p:P), Q p
+
+/-!#### Function typse as special cases of ∀ types
+what if the prop after the coma doesn't depend on the formal arg
+the output ignores the input, so it is simply a function
+-/
+#check ∀ (x : P), R
+
+-- this is now seen as [P → R : Prop]
+-- what about returning a data type based on input (fixed length list example)
+
+/-! ### Exists
+general form:
+example : ∃ (p : P), Q p := _
+for the proofs, we give first a witness (chosen example), then a proof that it satisfies the predicate
+using backslash angle brackets for the notation here
+-/
+-- toy examples:
+example : ∃ (n : Nat), n ≠ 0 := ⟨5, λ fneqz => nomatch fneqz⟩
+example : ∃ (n : Nat), isEven n := ⟨2, rfl⟩
+
+def foo : (∃ (n:Nat), isEven n) → True
+| ⟨ n', pf ⟩ => sorry --stubbed here to avoid error, but we never went further
+
+--elimination for existential generalizations: can't get a specific instance, but know that one exists
+
+
+/-! Elimination:
+if you know that there exists some number that is even, give a name to the nameless even example used to prove
+the existential proof forgot the info used to derive it though, no longer have access to the specific number
+-/
+
+/-!### Equality in Lean
+parameterized type with 2 inputs, yields a prop
+
+iductive Eq : α → α → Prop where
+| refl (a : α) : Eq a a
+
+the only proofs that can be built are things being equal to themselves
+use Eq all you want, but you can only prove it with refl
+-/
+#check 1 = 0
+#check Eq.refl 1
+
+--take a base case for even, that zero is even. add this to constructor
+inductive isEven' : Nat → Prop
+| zeven : isEven' 0
+| epte : ∀ (n : Nat), isEven' n → isEven' (n+2)
+
+example : isEven' 0 := isEven'.zeven
+example : isEven' 4 := isEven'.epte 2 (isEven'.epte 0 (isEven'.zeven))
